3a4fbdedd43335359fa6e3e537d9a1dbc8990e32
Rémi Bastide <Remi.Bastide@irit.fr>
2019-03-14 15:46:26 +0100

    Client ID

--- src/main/webapp/constants.js
+++ src/main/webapp/constants.js
@@ -1,8 +1,14 @@
+function randomString(length) {
+	var text = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for (var i = 0; i < length; i++)
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	return text;
+}
+
 var connectionInfo = {
 	host: "broker.mqttdashboard.com",
 	port: 8000,
-	clientID: "knobGaugeClient",
+	clientID: "knobGaugeClient-" + randomString(10),
 	topic: "bastide/grove/potentiometer"
 };
-
-
